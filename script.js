4351c36289dd7504052be2102b686e642dc2fd97
DaryaD <darya_gan_1989@mail.ru>
2023-05-08 10:10:16 +0200

    выполнение парактического задания по блоку 6

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Здравствуй, Мир!');
+const testTextField = document.querySelector('#testTextField');
+
+testTextField.addEventListener('click', function(event) {
+  // тело обработчика 1
+  this.textContent = 'Это событие:' + event.type;
+  console.log('x' + event.clientX, 'y' + event.clientY);
+  // конец тела обработчика 1
+})
+
+  
