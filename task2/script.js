1da6ee2da53aff1be3ca44671b4170e639fea615
DaryaD <darya_gan_1989@mail.ru>
2023-05-17 16:31:44 +0200

    налаживание работы task2 и создание task3

---
+++ task2/script.js
@@ -0,0 +1,17 @@
+const consoleLog = document.querySelector('#consoleLog');
+
+consoleLog.addEventListener('click', function () {
+   alert('Метод для вывода сообщения в веб-консоль');
+})
+
+const alertMsg = document.querySelector('#alert');
+
+alertMsg.addEventListener('click', function () {
+   alert('Позволяет вывести какую-либо информацию во всплывающем окне в браузере');
+})
+
+const promrtDg = document.querySelector('#prompt');
+
+promptDg.addEventListener('click', function () {
+   alert('Служит для вывода диалогового окна с сообщением, текстовым полем для ввода данных и кнопками «ОК» и «Отмена»');
+})
